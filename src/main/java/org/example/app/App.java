c9b58db958634e52349651f3f1a362a48a631b8d
LSielskiInterview <lsielski@sii.pl>
2024-07-09 13:53:20 +0200

    additional task

--- src/main/java/org/example/app/App.java
+++ src/main/java/org/example/app/App.java
@@ -1,7 +1,45 @@
 package org.example.app;
 
-public class App 
+import java.math.BigDecimal;
+import java.util.List;
+
+public class App
 {
-    public static void main( String[] args ){
+    static class Book {
+        private final BigDecimal price;
+        private final String name;
+
+        public Book(BigDecimal price, String name) {
+            this.price = price;
+            this.name = name;
+        }
+
+        public BigDecimal getPrice() {
+            return price;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public String toString() {
+            return "Book{" +
+                    "price=" + price +
+                    ", name='" + name + '\'' +
+                    '}';
+        }
     }
+    
+    public static void main(String[] args) {
+        final var booksList = List.of(
+                new Book(BigDecimal.valueOf(1.99), "Book 1"),
+                new Book(BigDecimal.valueOf(2.67), "Book 2"),
+                new Book(BigDecimal.valueOf(10.67), "Book 3"),
+                new Book(BigDecimal.valueOf(324.622), "Book 4"),
+                new Book(BigDecimal.valueOf(2.67), "Book 5"),
+                new Book(BigDecimal.valueOf(1.99), "Book 6")
+        );
+        // final var pricesToBooks = 
+    } 
 }
